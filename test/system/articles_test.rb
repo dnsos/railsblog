b3e1c1cfa31ff3089da62eb01c5ce8fa9e3e8e38
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2021-11-01 22:31:19 +0100

    test(Articles): system-test creating an article

--- test/system/articles_test.rb
+++ test/system/articles_test.rb
@@ -6,4 +6,17 @@ class ArticlesTest < ApplicationSystemTestCase
 
     assert_selector 'h1', text: 'Articles'
   end
+
+  test 'creating an article' do
+    visit articles_path
+
+    click_on 'New Article'
+
+    fill_in 'Title', with: 'A new test article'
+    fill_in 'Body', with: 'A new test body'
+
+    click_on 'Create Article'
+
+    assert_selector 'a', text: 'A new test article'
+  end
 end
